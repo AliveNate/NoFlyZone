c679f69cda178aa59fa6659f5e29118108fb1629
alivenate <alivenate@gmail.com>
2022-04-17 21:01:30 -0400

    Added images for buttons, and hover messages. A few new functions for hover, and image resizing.

--- BattleScribe GUI/Functions.py
+++ BattleScribe GUI/Functions.py
@@ -11,6 +11,42 @@ from PIL import Image, ImageTk
 # To let us browse/open a file after button click.
 from tkinter.filedialog import askopenfile
 # Local functions
+# Hover balloon messages
+from idlelib.tooltip import Hovertip
 
 
+def changeOnHover(button, colorOnHover, colorOnLeave):
+    # function to change the properties of a button on hover over it.
+    # adjust the background of the widget
+    # background on entering the widget
+    # bind is when the mouse enters into the widget i.e. hovers over
+    button.bind("<Enter>",  func=lambda e: button.config(background=colorOnHover))
+    # background color on leaving widget
+    button.bind("<Leave>", func=lambda e: button.config(background=colorOnLeave))
 
+
+def resizeButtonImage(address):
+    img = Image.open(address)
+    resize_image = img.resize((30, 38))
+    finalimage = ImageTk.PhotoImage(resize_image)
+    return finalimage
+
+
+'''
+# This is a convenient message box. Shows different options just keeping for future use if needed.
+from tkinter import *
+from tkinter import messagebox
+root = Tk()
+root.geometry("500x350")
+def msg1():
+    messagebox.showinfo('Show Information', 'Hi you got a prompt')
+    messagebox.showerror('Error', 'You got an error')
+    messagebox.showwarning('Warning', 'Do you accept the issue?')
+    messagebox.askquestion('Ask Question', 'Do you want to continue?')
+    messagebox.askokcancel('OK/Cancel', 'Are you sure?')
+    messagebox.askyesno('Yes/No', 'Do you want to continue?')
+    messagebox.askretrycancel('Retry', 'Failed! Do you want to try again?')
+
+Button(root, text="Click me", command=msg1).pack(pady=50)
+root.mainloop()
+'''
